30bd88c50c47d5d394146406e8bd9f245a736c14
N.Satoh <nsatoh_3_1415@hotmail.co.jp>
2018-06-09 11:06:06 +0900

    Add samples.

---
+++ samples/toi36_koch.py
@@ -0,0 +1,35 @@
+﻿# -*- coding: utf-8 -*-
+"""
+
+問36 コッホ曲線
+
+"""
+
+import turtlesvg as ttl
+t = ttl.MyTurtle()
+
+def koch(n, l):
+    if n == 0:
+        t.fd(l)
+    else:
+        koch(n-1, l/3)
+        # 向きを変える
+        koch(n-1, l/3)
+        # 向きを変える
+        koch(n-1, l/3)
+        # 向きを変える
+        koch(n-1, l/3)
+
+t.penup()
+t.goto(-300,-200)
+t.pendown()
+
+t.speed(0)
+#t.tracer(0)
+
+koch(4, 600)
+t.update()
+
+t.penup()
+t.save_as_svg('toi37_koch.svg')
+
